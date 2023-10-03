3a3aac62c4852f78d5c9fef58605c088123d4ef2
Jack <jckzhao@yahoo.com>
2023-10-02 23:09:14 -0400

    update
    figuring out pipelineextension for animation

---
+++ SpooninDrawer/AnimationData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpooninDrawer
+{
+    public class AnimationData
+    {
+        public int AnimationSpeed;
+        public bool IsLooping;
+        public List<AnimationFrameData> Frames;
+    }
+    public class AnimationFrameData
+    {
+        public int X;
+        public int Y;
+        public int CellWidth;
+        public int CellHeight;
+    }
+}
