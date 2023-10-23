90cb1bb32ca41abd79f344837f0b85b49d257807
Jack <jckzhao@yahoo.com>
2023-10-22 23:00:30 -0400

    update
    more XML testing

---
+++ XMLContent.Data/AnimationData.cs
@@ -0,0 +1,17 @@
+﻿namespace XMLContent.Data
+{
+    public class AnimationData
+    {
+        public int AnimationSpeed;
+        public bool IsLooping;
+        public List<AnimationFrameData> Frames;
+    }
+
+    public class AnimationFrameData
+    {
+        public int X;
+        public int Y;
+        public int CellWidth;
+        public int CellHeight;
+    }
+}
