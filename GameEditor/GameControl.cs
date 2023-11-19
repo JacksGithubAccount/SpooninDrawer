fb2ca59bd75c62e5e543ce2b0f1e5c0a047e9b78
Jack <jckzhao@yahoo.com>
2023-11-18 22:28:06 -0500

    update
    starting level editor

---
+++ GameEditor/GameControl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Forms.Controls;
+namespace GameEditor
+{
+    public class GameControl : MonoGameControl
+    {
+        private Texture2D _backgroundRectangle;
+        private OrthographicCamera _camera;
+        private bool _cameraDrag;
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        protected override void Draw()
+        {
+            var tower = Editor.Content.Load<Texture2D>(@"Sprites\Tower");
+            Editor.spriteBatch.Draw(
+             tower, new Rectangle(0, 0, tower.Width, tower.Height),
+            Color.White);
+            base.Draw();
+        }
+    }
+}
